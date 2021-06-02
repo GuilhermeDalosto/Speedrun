be54cccf186e57c26c793d76ed37388d86a712c4
Guilherme Dalosto <dalostoguilherme@gmail.com>
2021-06-02 16:59:40 -0300

    quick update

--- Scenes/Scenes/Login/LoginView.swift
+++ Scenes/Scenes/Login/LoginView.swift
@@ -8,5 +8,39 @@
 import UIKit
 
 class LoginView: UIView {
+        
+    lazy var titleLabel: UILabel = {
+        var view = UILabel(frame: .zero)
+        view.textColor = .black
+        return view
+    }()
     
+    var usernameField: UITextField = {
+        var view = UITextField(frame: .zero)
+        view.borderStyle = .roundedRect
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        makeDraw()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension LoginView: Drawable {
+    func setupHierarchy() {
+        addSubview(titleLabel)
+        addSubview(usernameField)
+    }
+    
+    func setupStyle() {}
+    
+    func setupConstraints() {}
+    
+    func setupAdditionalConfig() {}
 }
